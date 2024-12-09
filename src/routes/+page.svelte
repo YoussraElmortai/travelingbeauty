3115ddeaf7a206de84a235d95159b5fc7730aee7
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2024-12-09 14:56:12 +0100

    assets and styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,27 @@
 
+<section class="hero">
+ <img class="bgimage" src="/src/public/groupofpeople.avif" alt="wall" height="180" width="140" />
+ <h1>Bringing Welness to you</h1>
+ <h2></h2>
+</section>
+
 <section class="wrapper">
-    <div class="treatment"></div>
+  <div class="treatments">
+    <div class="treatment">
+    <h2>swedish massage</h2>
+    <div class="description">
+    <p>Relax and rejuvenate with our swedish massage services. Release tension, improve circulation, and enjoy a moment of pure tranquility in our soothing environment.</p>
+    <a class="link_secondary" href="/">Book Now</a></div>
+    </div>
+
+    <div class="treatment">
+    <h2>alignment massage</h2>
+    <div class="description">
+    <p>Relax and rejuvenate with our swedish massage services. Release tension, improve circulation, and enjoy a moment of pure tranquility in our soothing environment.</p>
+    <a class="link_secondary" href="/">Book Now</a></div>
+    </div>
+    </div>
+    <div class="link_wrapper">
+    <a class="link" href="/">more treatments</a>
+    </div>
 </section>
